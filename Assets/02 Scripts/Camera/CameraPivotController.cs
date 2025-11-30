30623a32d994c56f451e084662303029edadaf02
KSY <xaesoo30@gmail.com>
2025-11-30 14:07:06 +0900

    점프 불완전한 이슈 해결
    1. 기존의 코드를 조금 수정하였음
    2. 기존 코드에서 '코요테 타임' 적용 시켜서 해결되었음
        ㄴ (기억 안나면 Character Controller 코요테 타임 참조 할것)

---
+++ Assets/02 Scripts/Camera/CameraPivotController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraPivotController : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    private void CameraRotation()
+    {
+
+    }
+
+    private void CameraMove()
+    {
+
+    }
+}
--- Assets/02 Scripts/Character/Character Move.cs
+++ Assets/02 Scripts/Character/Character Move.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+   using UnityEngine;
 
 /// <summary>
 /// 점프, WASD기반 움직임, 마우스 
@@ -8,58 +8,68 @@ public class CharacterMove: MonoBehaviour
     private CharacterController controller;
     private Vector3 velocity;
 
-    [SerializeField] private float moveSpeed = 1f; // 움직이는 속도
+    private float lastGroundTime;
 
-    [SerializeField] private float rotationSpeed = 150f; // 캐릭터 회전 속도
-
+    [Header("Settings")]
+    [SerializeField] private float moveSpeed = 1f;   // 움직이는 속도
+    [SerializeField] private float rotationSpeed = 150f; // 캐릭터 회전 속도 
     [SerializeField] private float gravity = -9.81f; // 중력 값 (지구 중력 기준)
-    [SerializeField] private float jumpHeight = 2f; // 점프 높이 값
+    [SerializeField] private float jumpHeight = 2f;  // 점프 높이 값
+    [SerializeField] private float groundedBufferTime = 0.15f; // 점프 버퍼 시간 값
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
     }
     private void Update()
-    { 
-        Move();
+    {
         Rotation();
         Jump();
+        Move();
     }
 
     /// <summary>
     /// 캐릭터의 Position을 W,A,S,D 키로 조작하는 기능
     /// </summary>
-    public void Move()
+    private void Move()
     {
         var X = Input.GetAxis("Horizontal");
         var Z = Input.GetAxis("Vertical");
 
         Vector3 moveDir = transform.right * X + transform.forward * Z;
-        controller.Move(moveDir * moveSpeed * Time.deltaTime);
+        controller.Move(((moveDir * moveSpeed) + velocity) * Time.deltaTime);
     }
 
     /// <summary>
     /// 'Space Bar' 를 이용한 점프 기능
     /// </summary>
-    public void Jump()
+    private void Jump()
     {
-        if(controller.isGrounded && velocity.y < 0)
-            velocity.y = -2;
-        if (controller.isGrounded && Input.GetButtonDown("Jump"))
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        if (controller.isGrounded)
+        {
+            lastGroundTime = Time.time;
 
+            if (velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && (Time.time - lastGroundTime < groundedBufferTime))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            lastGroundTime = 0;
+        }
+            
         velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
     }
 
     /// <summary>
     /// 마우스 이동시 캐릭터 회전(Y축 기준 회전) 기능
     /// </summary>
-    public void Rotation()
+    private void Rotation()
     {
         float mouseX = Input.GetAxis("Mouse X");
         transform.Rotate(Vector3.up, mouseX * rotationSpeed * Time.deltaTime);
     }
-
-
 }

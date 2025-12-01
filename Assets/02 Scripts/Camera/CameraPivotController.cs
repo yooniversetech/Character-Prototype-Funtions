d0fae387d87145ef1804fca7586ce25b0c77bb5c
KSY <xaesoo30@gmail.com>
2025-12-01 20:39:29 +0900

    카메라 Y축 기준 회전 구현 완료
    1. 계층구조 = 캐릭터 -> 카메라 피봇(빈게임오브젝트) -> 메인 카메라순으로 만들어 새로운 스크립트인 CameraPivotController를 카메라 피봇의 컴포넌트로 추가해주었음

--- Assets/02 Scripts/Camera/CameraPivotController.cs
+++ Assets/02 Scripts/Camera/CameraPivotController.cs
@@ -2,20 +2,30 @@ using UnityEngine;
 
 public class CameraPivotController : MonoBehaviour
 {
-    private Camera mainCamera;
+    [Header("References")]
+    [SerializeField] private Transform playerBody;            // 캐릭터의 목이나 머리위치
+
+    [Header("Settings")]
+    private float mouseSensitivity = 150f;  // 마우스 감도
+    private float xRotation = 0f;           // 현제 위아래 각도
 
     private void Start()
     {
-        mainCamera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void CameraRotation()
+    private void Update()
     {
-
+        CameraYRotation();
     }
 
-    private void CameraMove()
+    private void CameraYRotation()
     {
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
 }
